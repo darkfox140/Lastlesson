2639aba9959e8268f81bcb1040a307f1ae925c98
darkfox140 <dark_fox@mail.ru>
2020-07-14 10:29:48 +0300

    Добавлен class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,17 @@ class LoginPageLocators():
     LOGIN_URL = (By.CSS_SELECTOR, '#login_link')
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, '#add_to_basket_form .btn')
+    MESSAGE_ADD_TO_BASKET = (By.CSS_SELECTOR, '#messages div:nth-child(1)')
+    PRODUCT_NAME = (By.CSS_SELECTOR, 'div.col-sm-6.product_main h1')
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, '#messages div:nth-child(1) div strong')
+    MESSAGE_PRICE = (By.CSS_SELECTOR, '.alert.alert-safe.alert-noicon.alert-info.fade.in div p:nth-child(1)')
+    PRICE_PRODUCT = (By.CSS_SELECTOR, 'div.col-sm-6.product_main p.price_color')
+    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, '.in div p:nth-child(1) strong')
+
+
+
+
