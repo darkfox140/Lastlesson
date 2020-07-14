8a421e4ca69ee307afc60351811ce092d7dcae17
darkfox140 <dark_fox@mail.ru>
2020-07-14 16:20:05 +0300

    Добавлены новые файлы

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,13 +8,14 @@ class ProductPage(BasePage):
         add_to_basket.click()
 
     def should_product_in_basket(self):
-        assert self.is_element_present(*ProductPageLocators.MESSAGE_ADD_TO_BASKET), 'Message add to basket is not presented'
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_ADD_TO_BASKET), \
+            'Message add to basket is not presented'
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         product_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET).text
-        assert product_name == product_in_basket
+        assert product_name == product_in_basket, 'Name is not same'
 
     def should_be_product_price(self):
-        assert self.is_element_present(*ProductPageLocators.MESSAGE_PRICE), 'Message price is not presented!'
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_PRICE), 'Message price is not presented'
         product_price = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
         product_price_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_BASKET).text
-        assert product_price == product_price_in_basket
+        assert product_price == product_price_in_basket, 'Price is not sam'
