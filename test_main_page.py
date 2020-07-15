e5684d1c8aed279f638fba972de2b4dc7a7d1a73
darkfox140 <dark_fox@mail.ru>
2020-07-15 18:13:34 +0300

    Изменен с учётом обновления файлов.

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-from .pages.main_page import MainPage
+from .pages.main_page import BasePage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 import pytest
@@ -6,7 +6,7 @@ import pytest
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
@@ -15,7 +15,7 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
 
