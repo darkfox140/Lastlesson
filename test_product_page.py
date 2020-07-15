6c4d9b83bf07d203419dcd6f8bd99b0738c0b4de
darkfox140 <dark_fox@mail.ru>
2020-07-15 18:12:11 +0300

    Добавлены новые тесты в новый файл.

---
+++ test_product_page.py
@@ -0,0 +1,17 @@
+from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_page()
