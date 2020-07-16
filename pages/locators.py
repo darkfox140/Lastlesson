c74e7ded10e00c3ea18f110a15e9575b2396e7c6
darkfox140 <dark_fox@mail.ru>
2020-07-16 13:51:38 +0300

    1. Добавленн файл basket_page.py в него добвленны соответствующие проверки 2. В base_page.py добавленны новые функции 3. В locators.py добавленн class BasketPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,17 +1,17 @@
 from selenium.webdriver.common.by import By
 
 
-class BasePageLocators():
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
 
 
-class ProductPageLocators():
+class ProductPageLocators:
     ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
     MESSAGE_ADD_TO_BASKET = (By.CSS_SELECTOR, '#messages :nth-child(1) div')
     PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
@@ -21,7 +21,7 @@ class ProductPageLocators():
     PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, '#messages p:nth-child(1) strong')
 
 
-class BasketPageLocators():
+class BasketPageLocators:
     VIEW_BASKET = (By.CSS_SELECTOR, 'span a')
     ITEMS_IN_BASKET = (By.CSS_SELECTOR, '#basket_formset div')
-    BASKET_EMPTY = (By.CSS_SELECTOR, '#content_inner p')
+    BASKET_EMPTY = (By.CSS_SELECTOR, '#content_inner p a')
